2609d69e3cfb1ee0692ff80b379b0f7ca56aa419
Manish Panwar <manish.panwar40@gmail.com>
2024-03-28 00:27:44 +0400

    all code

--- temp.py
+++ temp.py
@@ -1,8 +1,22 @@
-#------Tuples------#
-#Tuples means immutable i.e. cannot be changed
-num_array = [1,2,3,4] #array
-tuple_num = (1,2,3,4) #() means it's tuple i.e. cannot be changed
-num_array[0] = 10
-print(num_array)
-tuple_num[0] = 10
-print(tuple_num)
+
+def count_anagrams(s):
+    words = s.split()  # Split the string into words on the basis of whitespaces
+    anagram_count = 0  # Initialize the count of anagrams
+    # Iterate through each word
+    for i in range(len(words)):
+        # Skip words consisting of repeated characters only
+        if len(set(words[i])) == 1: #set() function returns only unique values in array
+            continue
+        
+        for j in range(i + 1, len(words)):
+            # Check if the words are anagrams and not the same word
+            if (sorted(words[i]) == sorted(words[j])) and (words[i] != words[j]):
+                anagram_count += 1
+                # if len(words[i]) == 1 and len(words[j]) == 1:
+                #     anagram_count += 1
+                # # elif words[i] != words[j]:
+                #     anagram_count += 1
+    return anagram_count
+# Test cases
+print(count_anagrams("aa aa odg dog gdo"))  # Output: 2
+print(count_anagrams("a c b c run urn urn"))  # Output: 1
